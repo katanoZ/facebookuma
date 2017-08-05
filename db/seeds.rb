3d9c097f76202fb3fbb9e33346e3c6f6878f8a1e
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2017-08-05 14:08:37 +0900

    [add]シードデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do |n|
+  name = Faker::Name.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(email: email,
+    name: name,
+    password: password,
+    password_confirmation: password,
+    )
+end
+
+1.upto 100 do |n|
+  title = Faker::Book.title
+  content = Faker::Hacker.say_something_smart
+  Topic.create!(
+    title: title,
+    content: content,
+    user_id: n,
+    )
+end
