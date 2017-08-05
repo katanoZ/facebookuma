3732b026322a53992cf9105979b3a7cc4126f85b
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2017-08-05 13:53:42 +0900

    [add]UserとTopicのリレーションを追加

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -21,6 +21,7 @@ class TopicsController < ApplicationController
 
   def create
     @topic = Topic.new(topic_params)
+    @topic.user_id = current_user.id
     if @topic.save
       redirect_to topics_path, notice: "トピックを作成しました"
     else
