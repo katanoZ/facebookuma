b149ef26ca505e59f4bfadfea39a30953959b654
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2017-08-14 19:06:29 +0900

    [add]フォロー機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'relationships/create'
+
+  get 'relationships/destroy'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root "top#index"
 
@@ -11,6 +15,10 @@ Rails.application.routes.draw do
   resources :topics do
     resources :comments
   end
+
+  resources :users, only: [:index, :show]
+
+  resources :relationships, only: [:create, :destroy]
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
