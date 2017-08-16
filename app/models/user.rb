9a259a95b92b6bf0e52d46c8ca97b874403650ce
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2017-08-16 17:56:58 +0900

    [add]devise関連のフォームをデザイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
   has_many :followed_users, through: :relationships, source: :followed
   has_many :followers, through: :reverse_relationships, source: :follower
 
+  validates :name, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
