7965c3d23eb62c78ecab5b682c808bedceef47ff
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2017-08-27 16:34:13 +0900

    [add]くま語のバリデーションを追加

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -5,7 +5,7 @@ class Topic < ActiveRecord::Base
   mount_uploader :image, ImageUploader
 
   validates :title, :content, presence: true
-
+  validates :content, format: {with: /[(くま)(クマ)(熊)(bear)(ぱんだ)(パンダ)(panda)]/, message: "には「くま」「クマ」「熊」などの語句を含めてください ʕ•ᴥ•ʔ"}
   default_scope {
     Topic.order(:id)
   }
