bee4537bb99dff89715f67c9a2c07ea994f53cb6
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2017-08-31 23:13:50 +0900

    [add]非同期処理のデプロイ用設定を追加

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,12 @@
 server '52.192.86.216', user: 'app', roles: %w{app db web}
 set :ssh_options, keys: '/Users/katanosakahiroaki/.ssh/id_rsa'
+
+require "delayed/recipes"
+
+role :delayed_job, '52.192.86.216' # ワーカ用サーバの指定
+set :delayed_job_server_role, :delayed_job   # delayed_jobのワーカを動かすロール名の設定
+set :delayed_job_command, defer { "#{bundle_cmd} exec bin/delayed_job" }
+
+after "deploy:stop",    "delayed_job:stop"
+after "deploy:start",   "delayed_job:start"
+after "deploy:restart", "delayed_job:restart"
